07f95c4f80b2c00aca412d5867d6ce6da93ff3dd
f-fardin-dev <fakurian.fardin@gmail.com>
2024-02-04 16:45:04 +0330

    chore: simulate the session and admin user

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,10 @@ import { links } from "./links";
 import { NavItem } from "./navItem/NavItem";
 import styles from "./navbar.module.css";
 
+//temporary
+const isAdmin = true;
+const session = true;
+
 export const Navbar = () => {
   return (
     <div className={styles.container}>
@@ -10,6 +14,14 @@ export const Navbar = () => {
         {links.map((link) => (
           <NavItem key={link.path} {...link} />
         ))}
+        {session ? (
+          <>
+            {isAdmin && <NavItem path="/admin" title="Admin" />}
+            <button className={styles.logoutBt}>Logout</button>
+          </>
+        ) : (
+          <NavItem path="/login" title="Login" />
+        )}
       </div>
     </div>
   );
