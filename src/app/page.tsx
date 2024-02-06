c7c1ddcea156c2889309eaba6ec682662a6e8a54
f-fardin-dev <fakurian.fardin@gmail.com>
2024-02-06 12:11:30 +0330

    feat: complete styles and content for the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1>Creative Thoughts Agency</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+          accusamus doloremque a, eaque odio rem aut quis.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brandsContainer}>
+          <Image
+            alt="Brands image"
+            src="/brands.png"
+            fill
+            className={styles.brandsImg}
+          />
+        </div>
+      </div>
+      <div className={styles.heroContainer}>
+        <Image
+          alt="Hero image"
+          src="/hero.gif"
+          fill
+          className={styles.heroImg}
+          objectFit="contain"
+        />
+      </div>
+    </div>
+  );
 }
