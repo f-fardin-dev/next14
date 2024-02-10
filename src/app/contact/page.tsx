9032663d075e1a309ffc1e40c05a3b3892a707ce
f-fardin-dev <fakurian.fardin@gmail.com>
2024-02-10 14:44:51 +0330

    feat: complete styles and content for the Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          fill
+          alt="Contact page image"
+          src="/contact.png"
+          objectFit="contain"
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name & Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea cols={30} rows={10} placeholder="Message" />
+          <button className={styles.formButton}>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
