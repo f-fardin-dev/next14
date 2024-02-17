977b2e764676175522ef0df846eb4b1493d9fe9f
f-fardin-dev <fakurian.fardin@gmail.com>
2024-02-17 15:54:24 +0330

    refactor: PostCard component
    Adding prop and using that to show data

--- src/components/postCard/PostCard.tsx
+++ src/components/postCard/PostCard.tsx
@@ -1,25 +1,34 @@
 import Image from "next/image";
 import styles from "./postCard.module.css";
 import Link from "next/link";
+import { IPost } from "@app/types/post.interface";
 
-export const PostCard = () => {
+interface PostCardProps {
+  post: IPost;
+}
+
+export const PostCard = ({
+  post: { title, description, slug, img },
+}: PostCardProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.postImageContainer}>
         <Image
           fill
           className={styles.postImage}
-          alt="post card image"
-          src="https://images.pexels.com/photos/18369349/pexels-photo-18369349/free-photo-of-tet-holiday-in-vietnam.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt={`image for: ${title}`}
+          src={img ?? "/defaultBlog.webp"}
           sizes="100%"
         />
       </div>
       <div className={styles.postInfo}>
-        <div className={styles.title}>Title</div>
-        <p className={styles.desc}>Description</p>
+        <div className={styles.title} title={title}>
+          {title}
+        </div>
+        <p className={styles.desc}>{description}</p>
         <div className={styles.extraInfo}>
           <small>01.01.2024</small>
-          <Link href="/blog/post">READ MORE &#10148;</Link>
+          <Link href={`/blog/${slug}`}>READ MORE &#10148;</Link>
         </div>
       </div>
     </div>
