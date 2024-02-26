87b786e44cc24f5038295df77408d7dd9697b16a
f-fardin-dev <fakurian.fardin@gmail.com>
2024-02-26 09:53:06 +0330

    fix: typo

--- src/services/db.ts
+++ src/services/db.ts
@@ -12,6 +12,6 @@ export const connectToDb = async () => {
     const db = await mongoose.connect(process.env.MONGO ?? "");
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
-    throw new Error(`Error coConnect to DB error: ${error}`);
+    throw new Error(`Error Connect to DB : ${error}`);
   }
 };
