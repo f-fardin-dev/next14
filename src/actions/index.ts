8b1a49d5ee352cd34a8cae6fa5d06afeb22299b9
f-fardin-dev <fakurian.fardin@gmail.com>
2024-02-26 10:29:15 +0330

    feat: add registerUser method (server action)

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { signIn, signOut } from "@app/auth";
+import { User } from "@app/models/users";
+import { connectToDb } from "@app/services/db";
 
 export const handleGithubLogin = async () => {
   await signIn("github");
@@ -8,4 +10,30 @@ export const handleGithubLogin = async () => {
 
 export const handleLogout = async () => {
   await signOut();
+};
+
+export const registerUser = async (formData: FormData) => {
+  const { username, email, password, passwordRepeat, img } =
+    Object.fromEntries(formData);
+
+  if (password !== passwordRepeat) {
+    return { error: "The passwords do not match." };
+  }
+  try {
+    connectToDb();
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return { error: "User already exists!" };
+    }
+
+    // const salt = await bcrypt.genSalt(10);
+    // const hPassword = await bcrypt.hash(password as string, salt);
+    const hPassword = `${password}&${username}`;
+    const newUser = new User({ username, email, img, password: hPassword });
+    await newUser.save();
+    console.log("User created");
+  } catch (error) {
+    return { error: "Something went wrong!" };
+  }
 };
