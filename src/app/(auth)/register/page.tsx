a27682a96c6c4ea3a17b24512c5290d6ba42543e
f-fardin-dev <fakurian.fardin@gmail.com>
2024-02-26 09:55:43 +0330

    feat: add the register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,38 @@
+import { registerUser } from "@app/actions";
+import styles from "./register.module.css";
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} action={registerUser}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          autoComplete="disable"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          autoComplete="disable"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          autoComplete="new-password"
+        />
+        <input
+          type="password"
+          placeholder="Repeat Password"
+          name="passwordRepeat"
+          autoComplete="new-password"
+        />
+        <button>Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
